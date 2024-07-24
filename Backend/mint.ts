80b2a9ada35a41ebc7c1b7ee224d0ce30dd708e2
Maheswaranx15 <maheshwaran66648@gmail.com>
2024-07-24 09:51:26 +0530

    contract modification and Backend chages

--- Backend/mint.ts
+++ Backend/mint.ts
@@ -3,7 +3,7 @@ const { abi } = require('./abi');
 
 // Usage
 async function main() {
-  let contractId = '0xcb10903dcc6cd70bb2368843b9872c22795fa2c0feb938e8c54a975d6283dc71';
+  let contractId = '0xa7828da4f0f86f988fd0d40c0f5dc2ba2ff5e18b0dc81d57317b77856938f3d0';
   // Create a provider, with the Latest Testnet URL.
   const provider = await Provider.create('https://testnet.fuel.network/v1/graphql');
   // Initialize wallet with a private key
@@ -15,20 +15,18 @@ async function main() {
   console.log("Value of Total Assets:", Number(value));
   const amount = BigInt(1);
   const recepient = {
-    bits: '0x1b29c3f4d418eceaee016dd42704287630e58ab01ce9e8f0fcd0c398b7a2033d'
+    bits: '0xde7d67cfa349db975d3781cd1e55b559ba71a3a37d8877825a1a9e703ac09cb4'
   };
   const subId = getRandomB256();
   console.log(recepient,subId,amount)
   const result = await contract.functions.mint({ Address: recepient }, subId, amount).txParams({ gasPrice: 1 }).call();
   console.log("Mint result:", await result.waitForResult());
-
-
   let getOwner = await contract.functions.owner().get()
   console.log(getOwner.value)
 
   const ownerAddress = {
     Address: {
-      bits: '0x49eae041aa67d832f3f55b210a2f538959c15168934adc040fcd23a5f3d04860'
+      bits: '0x1b29c3f4d418eceaee016dd42704287630e58ab01ce9e8f0fcd0c398b7a2033d'
     }
   };
 
--- Backend/set-metadata.ts
+++ Backend/set-metadata.ts
@@ -4,7 +4,7 @@ const { Provider, Wallet, Contract, getRandomB256 } = require('fuels');
 const { abi } = require('./abi');
 
 async function main() {
-  let contractId = '0xcb10903dcc6cd70bb2368843b9872c22795fa2c0feb938e8c54a975d6283dc71';
+  let contractId = '0xa7828da4f0f86f988fd0d40c0f5dc2ba2ff5e18b0dc81d57317b77856938f3d0';
   // Create a provider, with the Latest Testnet URL.
   const provider = await Provider.create('https://testnet.fuel.network/v1/graphql');
 
@@ -20,35 +20,34 @@ async function main() {
   console.log("Value of Total Assets:", Number(value));
 
   // // Define assetId and metadataKey
-  const assetId = { bits: '0x983069a830b1a9fbfc258470d41e73c11871ec48a062ca3a34e5e594bc63b43b' }; // Replace with actual asset ID
+  const assetId = { bits: '0xe9adcec50b7d3d47705ae07ca5cb02ab94b2d97ad9b94ac9fa57d5d45f0f2d30' }; // Replace with actual asset ID
   let subID = '' //get sub id from asset mint transaction
   // const mintedAssetId = getMintedAssetId(subID, contract.id.toB256());
   const metadataKey = 'image:png';
-  const keyvalue = 'https://arweave.net/fKgGBtyEk4XUo07r9sCMNrIlwOguL4aax6rMXFRJHJQ'
-
+  const keyvalue = 'https://arweave.net/LxILnA3lG-oRW5eZR4XAB-RnzpcFRjhSwh_PfQmw1Jw'   //--NO NFT
+  // https://arweave.net/fKgGBtyEk4XUo07r9sCMNrIlwOguL4aax6rMXFRJHJQ -- Yes 
   // Create the Metadata::String variant
   const metadataEnum = { String: keyvalue };
-  let Nftname = 'Cryptostage-NFT'
+  let Nftname = 'CryptoSage-NoNFT'
   let nftsymbol = 'CRYP-NO'
-  
   // Call set_metadata function with the correct parameters
-  // const setmetadata = await contract.functions.set_metadata(assetId,metadataKey,metadataEnum).txParams({ gasPrice: 1 }).call();
-  // let setmetadatatxn = await setmetadata.waitForResult();
-  // console.log("Metadata:", setmetadatatxn);
+  const setmetadata = await contract.functions.set_metadata(assetId,metadataKey,metadataEnum).txParams({ gasPrice: 1 }).call();
+  let setmetadatatxn = await setmetadata.waitForResult();
+  console.log("Metadata:", setmetadatatxn);
 
-  // const getmetadata = await contract.functions.metadata(assetId, metadataKey).get();
-  // console.log("Metadata:",getmetadata.value);
+  const getmetadata = await contract.functions.metadata(assetId, metadataKey).get();
+  console.log("Metadata:",getmetadata.value);
 
-  // const setname = await contract.functions.set_name(assetId,Nftname).txParams({ gasPrice: 1 }).call();
-  // let setnamedata = await setname.waitForResult();
-  // console.log("setnamedata:",setnamedata) 
+  const setname = await contract.functions.set_name(assetId,Nftname).txParams({ gasPrice: 1 }).call();
+  let setnamedata = await setname.waitForResult();
+  console.log("setnamedata:",setnamedata) 
 
-  // const setsymbol = await contract.functions.set_symbol(assetId,nftsymbol).txParams({ gasPrice: 1 }).call();
-  // let setsymboldata = await setsymbol.waitForResult();
-  // console.log("setsymboldata:",setsymboldata);
+  const setsymbol = await contract.functions.set_symbol(assetId,nftsymbol).txParams({ gasPrice: 1 }).call();
+  let setsymboldata = await setsymbol.waitForResult();
+  console.log("setsymboldata:",setsymboldata);
 
-  // const getsymbol = await contract.functions.symbol(assetId).get()
-  // console.log("Symbol of the Asset: ",getsymbol.value)
+  const getsymbol = await contract.functions.symbol(assetId).get()
+  console.log("Symbol of the Asset: ",getsymbol.value)
 
   const getname = await contract.functions.name(assetId).get()
   console.log("Name of the Asset: ",getname.value)
