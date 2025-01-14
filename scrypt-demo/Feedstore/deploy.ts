117ae6211f846eae42860c8258a0d4771c03db14
Maheswaranx15 <maheshwaran66648@gmail.com>
2025-01-14 09:20:32 +0530

    removed unusde code

--- scrypt-demo/Feedstore/deploy.ts
+++ scrypt-demo/Feedstore/deploy.ts
@@ -1,117 +1,3 @@
-// import { HashedSetState } from './src/contracts/usestate'
-// import {
-//     bsv,
-//     TestWallet,
-//     DefaultProvider,
-//     HashedSet,
-//     ByteString,
-//     toByteString,
-//     MethodCallOptions,
-// } from 'scrypt-ts'
-
-// import * as dotenv from 'dotenv'
-
-// // Load the .env file
-// dotenv.config()
-
-// if (!process.env.PRIVATE_KEY) {
-//     throw new Error(
-//         'No "PRIVATE_KEY" found in .env, Please run "npm run genprivkey" to generate a private key'
-//     )
-// }
-
-// // Read the private key from the .env file.
-// const privateKey = bsv.PrivateKey.fromWIF(process.env.PRIVATE_KEY || '')
-
-// // Prepare signer.
-// const signer = new TestWallet(
-//     privateKey,
-//     new DefaultProvider({
-//         network: bsv.Networks.testnet,
-//     })
-// )
-
-// async function compileContract() {
-//     try {
-//         console.log('Compiling the contract...');
-//         await HashedSetState.compile();
-//         console.log('Contract compiled successfully.');
-//     } catch (error) {
-//         console.error('Error compiling the contract:', error);
-//         throw error;
-//     }
-// }
-
-// async function deploy() {
-//     await compileContract();
-//     await HashedSetState.loadArtifact()
-
-//     // Initialize contract state
-//     const set = new HashedSet<ByteString>()
-//     const stateSet = new HashedSetState(set)
-
-//     // Connect to a signer.
-//     await stateSet.connect(signer)
-
-//     // Define initial balance for the contract (in satoshis)
-//     const initBalance = 10000
-
-//     // Contract deployment.
-//     const deployTx = await stateSet.deploy(initBalance)
-//     console.log(`Usestate contract deployed: ${deployTx.id}`)
-
-//     // Output the contract address
-//     // console.log('Contract Address:', stateSet.address)
-// }
-
-
-// async function callAddOnChain(
-//     txId: string,
-//     atOutputIndex = 0
-// ): Promise<string> {
-//     // Fetch TX via provider and reconstruct contract instance.
-//     console.log('entry')
-//     const tx = await signer.connectedProvider.getTransaction(txId)
-//     await compileContract();
-//     console.log('tx',tx)
-//     const instance = HashedSetState.fromTx(tx, atOutputIndex)
-//     console.log('instance',instance)
-//     await instance.connect(signer)
-
-//     const nextInstance = instance.next()
-//     const initialMessage = toByteString(
-//         'https://gateway.pinata.cloud/ipfs/QmbsxrAkDexeQuu6cC1NoaJeG1oH1864gySVhbJk5nZ6ow/7001.mp4',
-//         true
-//     )
-//     nextInstance.add(initialMessage)
-
-//     const { tx: callTx } = await instance.methods.add({
-//         next: {
-//             instance: nextInstance,
-//             balance: instance.balance,
-//         },
-//     } as MethodCallOptions<HashedSetState>)
-//     return callTx.id
-// }
-
-// // Execute the deployment function
-
-// // deploy().catch(console.error)
-
-// callAddOnChain('3b66b002b4ab0c9e735e6b08f4a7441602ab42baccf92382be6d564afc108ddf',0)
-// // async function main() {
-
-// //     // let lastTxId = deploy().catch(console.error)
-// //     // await callAddOnChain(.toString(),)
-// // }
-
-// // (async () => {
-// //     await main()
-// // })()
-
-// // 3b66b002b4ab0c9e735e6b08f4a7441602ab42baccf92382be6d564afc108ddf  
-
-
 import { HashedSetState } from './src/contracts/usestate';
 import {
     bsv,
@@ -183,53 +69,53 @@ const signer = new TestWallet(
 // }
 async function callAddOnChain(txId: string, atOutputIndex = 0){
     // Compile the contract before using it
-    await compileContract();
+    // await compileContract();
 
     // console.log('Fetching transaction...');
     const tx = await signer.connectedProvider.getTransaction(txId);
-    const instance = HashedSetState.fromTx(tx, 0)
+    // const instance = HashedSetState.fromTx(tx, 0)
     // console.log('instance fetched:', instance);
-    instance.connect(signer)
+    // instance.connect(signer)
     // Fetch the current state of the hashedset from the transaction
-    const currentSet = new HashedSet<ByteString>();
-    currentSet.add(
-        toByteString(
-            'https://gateway.pinata.cloud/ipfs/QmbsxrAkDexeQuu6cC1NoaJeG1oH1864gySVhbJk5nZ6ow/7001.mp4',
-            true
-        )
-    );
+    // const currentSet = new HashedSet<ByteString>();
+    // currentSet.add(
+    //     toByteString(
+    //         'https://gateway.pinata.cloud/ipfs/QmbsxrAkDexeQuu6cC1NoaJeG1oH1864gySVhbJk5nZ6ow/7001.mp4',
+    //         true
+    //     )
+    // );
 
     const message = toByteString('https://gateway.pinata.cloud/ipfs/QmbsxrAkDexeQuu6cC1NoaJeG1oH1864gySVhbJk5nZ6ow/7001.mp4', true)
     const { tx: callTx } = await instance.methods.add(message)
     console.log('Demo contract "unlock" called: ', callTx.id)
 
     // Reconstruct the contract instance with the current state of the hashedset
-    // const instance = HashedSetState.fromTx(tx, atOutputIndex);
+    const instance = HashedSetState.fromTx(tx, atOutputIndex);
 
-    // console.log('Reconstructed contract instance with current state:', currentSet);
+    console.log('Reconstructed contract instance with current state:', currentSet);
 
     // Connect the instance to the signer
-    // await instance.connect(signer);
+    await instance.connect(signer);
 
     // Create the next instance with the updated state
-    // const nextInstance = instance.next();
-    // const newMessage = toByteString(
-    //     'https://gateway.pinata.cloud/ipfs/QmbsxrAkDexeQuu6cC1NoaJeG1oH1864gySVhbJk5nZ6ow/7002.mp4',
-    //     true
-    // );
-    // nextInstance.hashedset.add(newMessage); // Add the new state
+    const nextInstance = instance.next();
+    const newMessage = toByteString(
+        'https://gateway.pinata.cloud/ipfs/QmbsxrAkDexeQuu6cC1NoaJeG1oH1864gySVhbJk5nZ6ow/7002.mp4',
+        true
+    );
+    nextInstance.hashedset.add(newMessage); // Add the new state
 
     
     // Call the `add` method with the updated state
-    // const { tx: callTx } = await instance.methods.add(newMessage, {
-    //     next: {
-    //         instance: nextInstance,
-    //         balance: instance.balance,
-    //     },
-    // } as MethodCallOptions<HashedSetState>);
+    const { tx: callTx } = await instance.methods.add(newMessage, {
+        next: {
+            instance: nextInstance,
+            balance: instance.balance,
+        },
+    } as MethodCallOptions<HashedSetState>);
 
-    // console.log('Call transaction ID:', callTx.id);
-    // return callTx.id;
+    console.log('Call transaction ID:', callTx.id);
+    return callTx.id;
 }
 
 
