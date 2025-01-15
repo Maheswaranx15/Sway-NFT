1a89ef79828d29ad9e0f5e23ed429f870bff2dd7
Maheswaranx15 <maheshwaran66648@gmail.com>
2025-01-15 12:08:13 +0530

    removed unusde code

--- scrypt-demo/Feedstore/deploy.ts
+++ scrypt-demo/Feedstore/deploy.ts
@@ -34,16 +34,16 @@ const signer = new TestWallet(
 );
 
 // Function to compile the contract
-// async function compileContract() {
-//     try {
-//         console.log('Compiling the contract...');
-//         await HashedSetState.compile();
-//         console.log('Contract compiled successfully.');
-//     } catch (error) {
-//         console.error('Error compiling the contract:', error);
-//         throw error;
-//     }
-// }
+async function compileContract() {
+    try {
+        console.log('Compiling the contract...');
+        await HashedSetState.compile();
+        console.log('Contract compiled successfully.');
+    } catch (error) {
+        console.error('Error compiling the contract:', error);
+        throw error;
+    }
+}
 
 // Deploy function
 // async function deploy() {
@@ -69,13 +69,13 @@ const signer = new TestWallet(
 // }
 async function callAddOnChain(txId: string, atOutputIndex = 0){
     // Compile the contract before using it
-    // await compileContract();
+    await compileContract();
 
     // console.log('Fetching transaction...');
     const tx = await signer.connectedProvider.getTransaction(txId);
-    // const instance = HashedSetState.fromTx(tx, 0)
-    // console.log('instance fetched:', instance);
-    // instance.connect(signer)
+    const instance = HashedSetState.fromTx(tx, 0)
+    console.log('instance fetched:', instance);
+    instance.connect(signer)
     // Fetch the current state of the hashedset from the transaction
     // const currentSet = new HashedSet<ByteString>();
     // currentSet.add(
@@ -86,13 +86,13 @@ async function callAddOnChain(txId: string, atOutputIndex = 0){
     // );
 
     const message = toByteString('https://gateway.pinata.cloud/ipfs/QmbsxrAkDexeQuu6cC1NoaJeG1oH1864gySVhbJk5nZ6ow/7001.mp4', true)
-    const { tx: callTx } = await instance.methods.add(message)
-    console.log('Demo contract "unlock" called: ', callTx.id)
+    // const { tx: callTx } = await instance.methods.add(message)
+    // console.log('Demo contract "unlock" called: ', callTx.id)
 
     // Reconstruct the contract instance with the current state of the hashedset
-    const instance = HashedSetState.fromTx(tx, atOutputIndex);
+    // const instance = HashedSetState.fromTx(tx, atOutputIndex);
 
-    console.log('Reconstructed contract instance with current state:', currentSet);
+    // console.log('Reconstructed contract instance with current state:', currentSet);
 
     // Connect the instance to the signer
     await instance.connect(signer);
